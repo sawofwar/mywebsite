775cbda2f877ce195e1c437639ab02a972cae0d5
sawofwar <115488930+sawofwar@users.noreply.github.com>
2022-11-13 04:27:25 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+'strict only';
+
+// let input = document.querySelector('.input-one');
+
+// input.addEventListener('keypress', function (event) {
+//   if (event.key === 'Enter') {
+//     document.querySelector('.input-text').textContent = String(input.value);
+//   }
+// });
