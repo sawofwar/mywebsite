2beed0155c9f603fcfd0eb08b28a1c9035533d1a
sawofwar <115488930+sawofwar@users.noreply.github.com>
2022-11-19 07:12:09 +0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-'strict only';
-
-// let input = document.querySelector('.input-one');
-
-// input.addEventListener('keypress', function (event) {
-//   if (event.key === 'Enter') {
-//     document.querySelector('.input-text').textContent = String(input.value);
-//   }
-// });
