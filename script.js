c277e9f03d7fa2dc02b2ce8c26c4891017dea169
sawofwar <115488930+sawofwar@users.noreply.github.com>
2022-11-28 20:12:31 +0400

    added sidebar

--- script.js
+++ script.js
@@ -7,3 +7,29 @@
 //     document.querySelector('.input-text').textContent = String(input.value);
 //   }
 // });
+
+const folderButton = document.getElementById('folder-button');
+const udemy = document.querySelector('.udemy');
+const udemyList = document.querySelector('.udemy-list');
+const myProjects = document.querySelector('.my-projects');
+
+const sideBarToggle = function () {
+  if (udemy.classList.contains('hidden')) {
+    udemyList.classList.remove('hidden');
+    udemy.classList.remove('hidden');
+  } else {
+    udemyList.classList.add('hidden');
+    udemy.classList.add('hidden');
+  }
+};
+
+const myProjectsOrange = function () {
+  if (myProjects.classList.contains('orangered')) {
+    myProjects.classList.remove('orangered');
+  } else {
+    myProjects.classList.add('orangered');
+  }
+};
+
+folderButton.addEventListener('click', sideBarToggle);
+folderButton.addEventListener('click', myProjectsOrange);
