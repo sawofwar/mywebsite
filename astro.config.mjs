4e996fc31b6d640fa0c43ca4ad4e578a27320101
ConstantineF <warsawxx@gmail.com>
2023-01-19 14:52:08 +0400

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,1 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-import preact from "@astrojs/preact";
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [preact()]
-});
